b33d89ca8ad3587606256358b81a79e0ae10b2d2
seacrab808 <seacrab808@naver.com>
2025-07-05 22:18:51 +0900

    MethodNotation

--- src/main/scala/lecture/MethodNotaion.scala
+++ src/main/scala/lecture/MethodNotaion.scala
@@ -1,20 +1,22 @@
 package lecture
 
+import scala.language.postfixOps
+
 object MethodNotaion extends App {
   class MyNumber(var value: Int) {
     // infix notation - parameter : 1
     def add(x: Int): Int = x + this.value
     def subtract(x: Int): Int = this.value - x
 
-//    // prefix notation(unary notation)
-//    def unary_- : MyNumber = new MyNumber(-value)
-//    def unary_! : String = s"Sorry, not possible"
-//
-//    // postfix
-//    def isZero: Boolean = this.value == 0
-//
-//    // apply function
-//    def apply(): String = s"MyNumber is ${this.value}!"
+    // prefix notation(unary notation)
+    def unary_- : MyNumber = new MyNumber(-value)
+    def unary_! : String = s"Sorry, not possible"
+
+    // postfix
+    def isZero: Boolean = this.value == 0
+
+    // apply function
+    def apply(): String = s"MyNumber is ${this.value}!"
   }
 
   // Create an instance of the class
@@ -25,4 +27,15 @@ object MethodNotaion extends App {
   val result2 = myNumber subtract 8 // Equivalent to MyNumber.subtract(8)
   println(result1) // Output: 15
   println(result2) // Output: 2
+
+  // prefix notation
+  println(-myNumber.value)
+  println(!myNumber)
+
+  // postfix notation(Requires "import scala.language.postfixOps")
+  println(myNumber isZero)
+
+  // apply
+  println(myNumber.apply())
+  println(myNumber())
 }
