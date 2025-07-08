40a284ef10ca562f7905bf875d46f98d1ad31852
seacrab808 <seacrab808@naver.com>
2025-07-08 17:02:53 +0900

    add ex02 code

---
+++ src/main/scala/ex02/Implementation.scala
@@ -0,0 +1,27 @@
+package ex02
+
+object Implementation extends Template {
+
+  import Expr.*
+
+  def interp(expr: Expr, env: Env): Value = {
+
+  }
+
+  def freeIds(expr: Expr): Set[String] = {
+
+  }
+
+  def bindingIds(expr: Expr): Set[String] = {
+
+  }
+
+  def boundIds(expr: Expr): Set[String] = {
+
+  }
+
+  def shadowedIds(expr: Expr): Set[String] = {
+
+  }
+
+}
---
+++ src/main/scala/ex02/error.scala
@@ -0,0 +1,7 @@
+package ex02
+
+case class PLError(msg: String) extends Exception(msg)
+
+def error(): Nothing = error("")
+
+def error(msg: String): Nothing = throw PLError(msg)
