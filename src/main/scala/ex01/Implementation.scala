688a734188b5a6c223f9674f9b044b6b5b574429
seacrab808 <seacrab808@naver.com>
2025-07-08 13:30:34 +0900

    solve ex01 code

--- src/main/scala/ex01/Implementation.scala
+++ src/main/scala/ex01/Implementation.scala
@@ -4,7 +4,19 @@ object Implementation extends Template {
 
   import Expr.*
 
-  def interp(expr: Expr): Value = ???
+  def interp(expr: Expr): Value = {
+    expr match {
+      case Expr.Num(n) => n
+      case Expr.Add(l, r) => interp(l) + interp(r)
+      case Expr.Mul(l, r) => interp(l) * interp(r)
+    }
+  }
 
-  def countNums(expr: Expr): Int = ???
+  def countNums(expr: Expr): Int = {
+    expr match {
+      case Expr.Num(_) => 1
+      case Expr.Add(l, r) => countNums(l) + countNums(r)
+      case Expr.Mul(l, r) => countNums(l) + countNums(r)
+    }
+  }
 }
